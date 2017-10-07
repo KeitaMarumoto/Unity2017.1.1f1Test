4c8c8caa1dc8f3f929aadb04cfaa6e7d1f2aa7e3
KeitaMarumoto <marumoto.keita@gmail.com>
2017-10-07 18:58:03 +0900

    Skillのサンプル。

---
+++ Assets/Scripts/CEnamyBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CEnamyBase : MonoBehaviour {
+	#region メンバ変数
+	private float m_helthPoint;
+	private float m_attackPoint;
+	private List<CSkillBase> m_skills = new List<CSkillBase>();
+	#endregion
+
+	#region Monobehavior基底コールバック
+	// Use this for initialization
+	void Start () {
+		m_skills.Add(new CSkillA());
+		m_skills.Add(new CSkillB());
+
+		foreach(var skill in m_skills)
+		{
+			skill.Execute();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	#endregion
+}
---
+++ Assets/Scripts/CSkillBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSkillBase {
+
+	public virtual void Execute()
+	{
+		Debug.Log("Base.");
+	}
+}
+
+public class CSkillA : CSkillBase
+{
+	public override void Execute()
+	{
+		Debug.Log("SkillA.");
+	}
+}
+
+public class CSkillB : CSkillBase
+{
+	public override void Execute()
+	{
+		Debug.Log("SkillB.");
+	}
+}
